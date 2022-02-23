3efcc6d0bc63d31585ce3ccb2340afe0707a58e5
pm <pm@CM33L63.com>
2022-02-23 16:32:48 -0500

    initial commit - 2

---
+++ scr_test/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+// https://sites.harding.edu/fmccown/screensaver/screensaver.html
+
+
+namespace scr_test
+{
+    public partial class Form1 : Form
+    {
+        private bool isAlive = false;
+        private Point oldMouseLocation;
+        Graphics g = null;
+
+        // -------------------------------------------------------------------
+
+        public Form1(Rectangle bounds)
+        {
+            InitializeComponent();
+
+            //Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Bounds = bounds;
+            this.TopMost = true;
+            this.DoubleBuffered = true;
+
+            zzz2();
+        }
+
+        private void zzz1()
+        {
+            g = this.CreateGraphics();
+            isAlive = true;
+
+            new System.Threading.Tasks.Task(() =>
+            {
+                var list = new System.Collections.Generic.List<star>();
+
+                for (int i = 0; i < 500; i++)
+                {
+                    list.Add(new star(this.Width, this.Height));
+                }
+
+                while (isAlive)
+                {
+                    g.FillRectangle(Brushes.Black, 0, 0, this.Width, this.Height);
+
+                    foreach (var s in list)
+                    {
+                        g.FillRectangle(Brushes.Red, s.X, s.Y, s.size, s.size);
+                        s.move();
+                    }
+
+                    System.Threading.Thread.Sleep(10);
+                }
+
+                g.Dispose();
+
+            }).Start();
+        }
+
+        // Using form's background image as our drawing surface
+        private void zzz2()
+        {
+            Bitmap buffer = new Bitmap(this.Width, this.Height);    // set the size of the image
+            g = Graphics.FromImage(buffer);                         // set the graphics to draw on the image
+
+            isAlive = true;
+            this.BackgroundImage = buffer;                          // set the PictureBox's image to be the buffer
+
+            new System.Threading.Tasks.Task(() =>
+            {
+                var list = new System.Collections.Generic.List<star>();
+
+                for (int i = 0; i < 1500; i++)
+                {
+                    list.Add(new star(this.Width, this.Height));
+                }
+
+                while (isAlive)
+                {
+                    g.FillRectangle(Brushes.Black, 0, 0, this.Width, this.Height);
+
+                    foreach (var s in list)
+                    {
+                        g.FillRectangle(Brushes.Green, s.X, s.Y, s.size, s.size);
+                        s.move();
+                    }
+
+                    this.Invalidate();
+                    System.Threading.Thread.Sleep(33);
+                }
+
+            }).Start();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int dist = 50;
+
+            if (!oldMouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(oldMouseLocation.X - e.X) > dist || Math.Abs(oldMouseLocation.Y - e.Y) > dist)
+                {
+                    isAlive = false;
+                    g.Dispose();
+                    Application.Exit();
+                }
+            }
+
+            // Update mouse location
+            oldMouseLocation = e.Location;
+        }
+    }
+}
---
+++ scr_test/graph.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;   // For dll import
+
+public class star
+{
+    public int size;
+    public int X;
+    public int Y;
+
+    private int dx;
+    private int dy;
+
+    private int MaxX;
+    private int MaxY;
+
+    public star(int maxX, int maxY)
+    {
+        var rand = new Random();
+
+        MaxX = maxX;
+        MaxY = maxY;
+
+        X = rand.Next(maxX);
+        Y = rand.Next(maxY);
+
+        int speed = 20;
+
+        dx = (rand.Next(speed) + 1) * (rand.Next(2) == 0 ? 1 : -1);
+        dy = (rand.Next(speed) + 1) * (rand.Next(2) == 0 ? 1 : -1);
+
+        size = rand.Next(10) + 1;
+    }
+
+    public void move()
+    {
+        X += dx;
+        Y += dy;
+
+        if (X < 0 || X > MaxX)
+        {
+            dx *= -1;
+        }
+
+        if (Y < 0 || Y > MaxY)
+        {
+            dy *= -1;
+        }
+    }
+};
+
+public class myGraphics
+{
+    [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
+    private static extern IntPtr GetDC(IntPtr hWnd);
+
+    [DllImport("user32.dll", ExactSpelling = true)]
+    private static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+    [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+    private static extern IntPtr GetDesktopWindow();
+
+    public static void DrawBitmapToScreen(Bitmap bmp)
+    {
+        int width = bmp.Width;
+        int height = bmp.Height;
+
+        IntPtr hwnd = GetDesktopWindow();
+        IntPtr hdc = GetDC(hwnd);
+        using (Graphics g = Graphics.FromHdc(hdc))
+        {
+            g.DrawImage(bmp, new Point(0, 0));
+        }
+
+        ReleaseDC(hwnd, hdc);
+    }
+
+    public static void DrawBitmapToWindow(Bitmap bmp, IntPtr handle)
+    {
+        int width = bmp.Width;
+        int height = bmp.Height;
+
+        IntPtr hwnd = handle;
+        IntPtr hdc = GetDC(hwnd);
+        using (Graphics g = Graphics.FromHdc(hdc))
+        {
+            g.DrawImage(bmp, new Point(0, 0));
+        }
+
+        ReleaseDC(hwnd, hdc);
+    }
+
+}
